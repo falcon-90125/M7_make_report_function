12642e0c3bd220a29b6c8cd4f979704d432323eb
falcon-90125 <sokolov_aa@ngs.ru>
2022-09-24 23:16:53 +0700

    'popular_goods.v2'

---  index.py
+++  index.py
@@ -5,13 +5,16 @@ from collections import defaultdict, Counter
 from pprint import pprint
 from openpyxl import load_workbook
 from data_visits import data_visits
+from popular_goods import popular_goods
+
 # 7 самых популярных браузеров по посещаемости
 log_file_name = 'logs.xlsx'
 report_template_file_name = 'report_template.xlsx'
 report_output_file_name = 'report_template_output.xlsx'
 
-def make_report(log_file_name): #, report_template_file_name, report_output_file_name
+def make_report(log_file_name, report_template_file_name, report_output_file_name): #, report_template_file_name, report_output_file_name
     browser_data, browser_pop_list, month_list_counter = data_visits(log_file_name)
+    sales_goods_data, sales_months_data = popular_goods(log_file_name)
 
     # Открываем файл шаблона отчета report_template.xlsx
     wb = load_workbook(filename=report_template_file_name, data_only=True)
@@ -29,7 +32,11 @@ def make_report(log_file_name): #, report_template_file_name, report_output_file
             cell_all_in_month = ws.cell(row=12, column=2+month)
             cell_all_in_month.value = allInMonthVal[month+1]
 
+    for i, val in enumerate(sales_goods_data.keys()):
+        cell_i = ws.cell(row=19+i, column=1)
+        cell_i.value = val
+
     # Сохраняем файл-отчет
     wb.save(report_output_file_name)
 
-make_report(log_file_name)
+make_report(log_file_name, report_template_file_name, report_output_file_name)
