53ff0f0316f8a102e0d2266acb1cdc9eebd94225
falcon-90125 <sokolov_aa@ngs.ru>
2022-09-26 12:41:51 +0700

    'genders_goods_rework'

---  index.py
+++  index.py
@@ -10,16 +10,21 @@ log_file_name = 'logs.xlsx'
 report_template_file_name = 'report_template.xlsx'
 report_output_file_name = 'report_template_output.xlsx'
 
-def make_report(log_file_name, report_template_file_name, report_output_file_name): #, report_template_file_name, report_output_file_name
+def make_report(log_file_name, report_template_file_name, report_output_file_name):
+    # Посетители веб-сайта
     # browser_data - Данные по посещениям браузеров по месяцам
     # all_visits_pop_browser_months_dict - Словарь суммарных значений продаж по месяцам по всем популярным браузерам
     browser_data, all_visits_pop_browser_months_dict = data_visits(log_file_name)
+
+    # Популярные товары
     # sales_goods_data - Данные по продажам товаров по месяцам
     # all_sales_pop_goods_months_dict - Словарь суммарных значений продаж по месяцам по всем популярным товарам
     sales_goods_data, all_sales_pop_goods_months_dict = popular_goods(log_file_name)
-    # pop_mens_goods, pop_woomens_goods - Наиболее популярные товары
-    # no_pop_mens_goods, no_pop_woomens_goods - Наименее популярные товары
-    pop_mens_goods, pop_woomens_goods, no_pop_mens_goods, no_pop_woomens_goods = genders_goods(log_file_name)
+
+    # Предпочтения
+    # Наиболее популярные товары: goods_list[0]mens_goods, goods_list[2]woomens_goods
+    # Наименее популярные товары: goods_list[1]mens_goods, goods_list[3]woomens_goods
+    genders_goods_list = genders_goods(log_file_name)
 
     # Открываем файл шаблона отчета report_template_file_name.xlsx
     wb = load_workbook(filename=report_template_file_name, data_only=True)
@@ -50,10 +55,10 @@ def make_report(log_file_name, report_template_file_name, report_output_file_nam
             cell_all_month.value = all_sales_pop_goods_months_dict[month+1] # Записываем итоговое значение посещения в ячейку
 
     # Предпочтения
-    ws['B31'] = pop_mens_goods
-    ws['B32'] = pop_woomens_goods
-    ws['B33'] = no_pop_mens_goods
-    ws['B34'] = no_pop_woomens_goods
+    ws['B31'] = genders_goods_list[0] #pop_mens_goods
+    ws['B32'] = genders_goods_list[2] #pop_woomens_goods
+    ws['B33'] = genders_goods_list[1] #no_pop_mens_goods
+    ws['B34'] = genders_goods_list[3] #no_pop_woomens_goods
 
     # Сохраняем файл-отчет
     wb.save(report_output_file_name)
--- genders_goods.py
+++ genders_goods.py
@@ -1,7 +1,7 @@
 import collections
 import pandas as pd
 from collections import defaultdict, Counter
-
+log_file_name = 'logs.xlsx'
 def genders_goods(log_file_name):
     # Чтение и анализ данных из Excel, берём только колонки с наименованиями товаров и "Дата посещения"
     cols = ['Пол', 'Купленные товары']
@@ -26,26 +26,14 @@ def genders_goods(log_file_name):
             for i in range(len(good_list)): # Проходим по полученному списку наименований товаров
                 woomens_goods_list.append(good_list[i])
 
-    # Считаем количество вхождений наименований товаров в мужских и женских списках
-    letter_counter_mens_goods = collections.Counter(mens_goods_list)
-    letter_counter_woomens_goods = collections.Counter(woomens_goods_list)
+    genders_goods_list = []
+    for i in mens_goods_list, woomens_goods_list:
+        letter_counter_goods = collections.Counter(i)
+        # Методом most_common отсортируем товары по популярности в порядке убывания
+        letters_most_common = letter_counter_goods.most_common()
+        genders_goods_list.append(letters_most_common[0][0]) # Наиболее популярные товары
+        genders_goods_list.append(letters_most_common[:-(len(letter_counter_goods)+1):-1][0][0]) # Наименее популярные товары
 
-    # Методом most_common отсортируем товары по популярности в порядке убывания
-    letters_most_common_mens_goods = letter_counter_mens_goods.most_common()
-    letters_most_common_woomens_goods = letter_counter_woomens_goods.most_common()
+    # print(goods_list)
 
-    # Наиболее популярные товары
-    pop_mens_goods = letters_most_common_mens_goods[0][0]
-    pop_woomens_goods = letters_most_common_woomens_goods[0][0]
-
-    # print(pop_mens_goods)
-    # print(pop_woomens_goods)
-
-    # Наименее популярные товары
-    no_pop_mens_goods = letters_most_common_mens_goods[:-(len(letter_counter_mens_goods)+1):-1][0][0]
-    no_pop_woomens_goods = letters_most_common_woomens_goods[:-(len(letter_counter_woomens_goods)+1):-1][0][0]
-
-    # print(no_pop_mens_goods)
-    # print(no_pop_woomens_goods)
-
-    return pop_mens_goods, pop_woomens_goods, no_pop_mens_goods, no_pop_woomens_goods
+    return genders_goods_list
